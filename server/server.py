d0fca1e85b6b7a967e67cef5909373baa81d4a49
Daniel Cavalcante <danielcavalcante@protonmail.com>
2023-06-10 10:24:32 -0300

    Add minio storage simple upload

--- server/server.py
+++ server/server.py
@@ -1,23 +1,53 @@
+import os
+from typing import Any
 from flask import Flask, render_template, request
-from pathlib import Path
+from minio import Minio
 
 from werkzeug.utils import secure_filename
 
 TEMPORARY_FILE_DIR="/tmp/chunked-file-transfer"
+TEMPORARY_BUCKET_NAME="temporary_bucket"
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT") or "localhost:9000"
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY") or "minioadmin"
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY") or "minioadmin"
+MINIO_IS_SECURE = os.getenv("MINIO_IS_SECURE") or "False"
 
 app = Flask(__name__)
+storage = Minio(MINIO_ENDPOINT,
+                MINIO_ACCESS_KEY,
+                MINIO_SECRET_KEY,
+                MINIO_IS_SECURE.lower() in ['true'])
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+def upload_to_storage(bucket_name: str,
+                      object_name: str,
+                      data: Any) -> bool:
+    try:
+        if not storage.bucket_exists(bucket_name):
+            storage.make_bucket(bucket_name)
+        result = storage.put_object(bucket_name,
+                                    object_name,
+                                    data.stream, 
+                                    length=-1,
+                                    part_size=10*1024*1024)
+    except Exception:
+        raise
+    else:
+        return True if result else False
+
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
     assert file.filename is not None;
-    file.save(Path(TEMPORARY_FILE_DIR) / secure_filename(file.filename))
+    success = upload_to_storage(TEMPORARY_BUCKET_NAME,
+                                secure_filename(file.filename),
+                                data=file)
 
-    return { "success": True }, 200
+    return { "success": success }, 200
 
 
 if __name__ == "__main__":
