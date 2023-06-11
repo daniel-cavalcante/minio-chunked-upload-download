a044a5496381fabe0e7fd077e385444a6c738513
Daniel <97473619+daniel-cavalcante@users.noreply.github.com>
2023-06-11 17:58:05 -0300

    Add web interface to upload file (#2)

--- server/server.py
+++ server/server.py
@@ -6,8 +6,8 @@ from minio import Minio
 
 from werkzeug.utils import secure_filename
 
-TEMPORARY_FILE_DIR="/tmp/chunked-file-transfer"
-TEMPORARY_BUCKET_NAME="temporary_bucket"
+TEMPORARY_FILE_DIR = "/tmp/chunked-file-transfer"
+TEMPORARY_BUCKET_NAME = "temporary-bucket"
 
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT") or "localhost:9000"
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY") or "minioadmin"
@@ -16,14 +16,17 @@ MINIO_IS_SECURE = os.getenv("MINIO_IS_SECURE") or "False"
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-storage = Minio(MINIO_ENDPOINT,
-                MINIO_ACCESS_KEY,
-                MINIO_SECRET_KEY,
-                MINIO_IS_SECURE.lower() in ['true'])
+storage = Minio(endpoint=MINIO_ENDPOINT,
+                access_key=MINIO_ACCESS_KEY,
+                secret_key=MINIO_SECRET_KEY,
+                # accepts capitalization variants as in FALSE, False and false
+                secure=MINIO_IS_SECURE.lower() in ['true'])
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 def upload_to_storage(bucket_name: str,
                       object_name: str,
@@ -33,7 +36,7 @@ def upload_to_storage(bucket_name: str,
             storage.make_bucket(bucket_name)
         result = storage.put_object(bucket_name,
                                     object_name,
-                                    data.stream, 
+                                    data.stream,
                                     length=-1,
                                     part_size=10*1024*1024)
     except Exception:
@@ -41,15 +44,16 @@ def upload_to_storage(bucket_name: str,
     else:
         return True if result else False
 
+
 @app.route("/upload", methods=["POST"])
 def upload():
-    file = request.files["file"]
-    assert file.filename is not None;
+    file = request.files["myFile"]
+    assert file.filename is not None
     success = upload_to_storage(TEMPORARY_BUCKET_NAME,
                                 secure_filename(file.filename),
                                 data=file)
 
-    return { "success": success }, 200
+    return {"success": success}, 200
 
 
 if __name__ == "__main__":
