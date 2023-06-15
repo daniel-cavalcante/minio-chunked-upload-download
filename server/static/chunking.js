95c20646c80043d4d254f95715afd81c11c28537
Daniel <97473619+daniel-cavalcante@users.noreply.github.com>
2023-06-15 19:41:15 -0300

    Format html and js files

--- server/static/chunking.js
+++ server/static/chunking.js
@@ -1,58 +1,64 @@
-let file, fileId, fileName, fileSize, chunksTotal
-const CHUNK_SIZE = 5 * 1024 * 1024
-const BASE_URL = "http://localhost:5000"
+let file, fileId, fileName, fileSize, chunksTotal;
+const CHUNK_SIZE = 5 * 1024 * 1024;
+const BASE_URL = "http://localhost:5000";
 
-const input = document.getElementById("input")
+const input = document.getElementById("input");
+const button = document.getElementById("button");
 
 const handleChange = (event) => {
-    file = event.target.files[0]
-    fileId = crypto.randomUUID()
-    fileName = file.name
-    fileSize = file.size
-    chunksTotal = Math.ceil(fileSize / CHUNK_SIZE)
-}
+  file = event.target.files[0];
+  fileId = crypto.randomUUID();
+  fileName = file.name;
+  fileSize = file.size;
+  chunksTotal = Math.ceil(fileSize / CHUNK_SIZE);
+};
 
-input.addEventListener("change", handleChange)
+const handleClick = (event) => {
+  console.info("function is not implemented");
+};
+
+input.addEventListener("change", handleChange);
+button.addEventListener("click", handleClick);
 
 const createChunk = (file, index) => {
-    const isLastChunk = index + 1 == chunksTotal;
+  const isLastChunk = index + 1 == chunksTotal;
 
-    let chunk;
-    if (isLastChunk) {
-      chunk = file.slice(index * CHUNK_SIZE);
-    } else {
-      chunk = file.slice(index * CHUNK_SIZE, (index + 1) * CHUNK_SIZE);
-    }
+  let chunk;
+  if (isLastChunk) {
+    chunk = file.slice(index * CHUNK_SIZE);
+  } else {
+    chunk = file.slice(index * CHUNK_SIZE, (index + 1) * CHUNK_SIZE);
+  }
 
-    const chunkForm = new FormData();
-    const chunkName = fileName + '.chunk' + index;
-    chunkForm.append('chunk', chunk, chunkName);
-    chunkForm.append('chunkIndex', index.toString());
-    chunkForm.append('chunkSize', chunk.size.toString());
-    chunkForm.append('chunksTotal', chunksTotal.toString());
-    chunkForm.append('fileId', fileId);
-    chunkForm.append('fileName', fileName);
-    chunkForm.append('fileSize', fileSize.toString());
+  const chunkForm = new FormData();
+  const chunkName = fileName + ".chunk" + index;
+  chunkForm.append("chunk", chunk, chunkName);
+  chunkForm.append("chunkIndex", index.toString());
+  chunkForm.append("chunkSize", chunk.size.toString());
+  chunkForm.append("chunksTotal", chunksTotal.toString());
+  chunkForm.append("fileId", fileId);
+  chunkForm.append("fileName", fileName);
+  chunkForm.append("fileSize", fileSize.toString());
 
-    return chunkForm;
+  return chunkForm;
 };
 
 const uploadChunk = async (chunkForm, index) => {
-    const headers = new Headers();
-    headers.append("Content-Type", "multipart/form-data")
+  const headers = new Headers();
+  headers.append("Content-Type", "multipart/form-data");
 
-    const init = {
-        method: "POST",
-        mode: "cors",
-        headers: headers,
-        body: chunkForm
-    }
+  const init = {
+    method: "POST",
+    mode: "cors",
+    headers: headers,
+    body: chunkForm,
+  };
 
-    const response = await fetch(BASE_URL + '/upload', init);
+  const response = await fetch(BASE_URL + "/upload", init);
 
-    if (response.status == 201) {
-        console.info(`chunk #${index} was uploaded successfully`);
-    } else {
-        console.error(`upload of chunk #${index} has failed`);
-    }
+  if (response.status == 201) {
+    console.info(`chunk #${index} was uploaded successfully`);
+  } else {
+    console.error(`upload of chunk #${index} has failed`);
+  }
 };
