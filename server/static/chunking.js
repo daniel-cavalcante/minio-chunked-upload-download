04429298f459bf1094cee8a68c0b22c9fa7819f6
Daniel <97473619+daniel-cavalcante@users.noreply.github.com>
2023-06-16 14:33:04 -0300

    Feature/clientside chunking (#4)
    * Set up script in html
    
    * Add event listener and createChunk function
    
    * Add uploadChunk function
    
    * Format html and js files
    
    * Refactor chunking.js to use XMLHTTPRequest

---
+++ server/static/chunking.js
@@ -0,0 +1,82 @@
+// constants and the global variables
+const CHUNK_SIZE = 5 * 1024 * 1024;
+const BASE_URL = "http://localhost:5000";
+let file;
+
+// code that handles user selecting a file
+const handleChange = (event) => {
+  file = event.target.files[0];
+};
+
+const input = document.getElementById("fileInput");
+input.addEventListener("change", handleChange);
+
+// code that handles user clicking on 'send' button
+const handleClick = () => {
+  // set total number of chunks
+  const chunksTotal = Math.ceil(file.size / CHUNK_SIZE);
+
+  // set file identifier to be used by the server to handle exceptions
+  const fileId = crypto.randomUUID();
+
+  for (let chunkIndex = 0; chunkIndex < chunksTotal; chunkIndex++) {
+    // each loop picks a chunk of file to be sent to the server
+    let chunk;
+    const isLastChunk = chunkIndex + 1 == chunksTotal;
+    if (isLastChunk) {
+      // the last chunk is simply what is left of the file
+      chunk = file.slice(chunkIndex * CHUNK_SIZE);
+    } else {
+      chunk = file.slice(
+        chunkIndex * CHUNK_SIZE,
+        (chunkIndex + 1) * CHUNK_SIZE
+      );
+    }
+
+    const chunkMetaData = {
+      chunkIndex: chunkIndex,
+      chunksTotal: chunksTotal,
+      originalFileId: fileId,
+      originalFileName: file.name,
+      originalFileSize: file.size,
+    };
+
+    const chunkFormData = getFormData(chunk, chunkMetaData);
+
+    sendData(chunkFormData);
+  }
+};
+
+const button = document.getElementById("sendButton");
+button.addEventListener("click", handleClick);
+
+const getFormData = (chunk, metaData) => {
+  const chunkForm = new FormData();
+
+  for (const [key, value] of Object.entries(metaData)) {
+    chunkForm.append(key, value);
+  }
+  // append the slice blob with a unique name indicating its part
+  const chunkName = metaData.originalFileName + ".part" + metaData.chunkIndex;
+  chunkForm.append("chunk", chunk, chunkName);
+
+  return chunkForm;
+};
+
+const sendData = (formData) => {
+  const XHR = new XMLHttpRequest();
+
+  // code that listens for success or exceptions and handles it
+  XHR.addEventListener("load", (event) => {
+    console.info(event.target.status, event);
+  });
+
+  XHR.addEventListener("error", (event) => {
+    console.error(event.target.status, event);
+  });
+
+  // sets up the request then sends the form
+  // HTTP headers are set automatically
+  XHR.open("POST", BASE_URL + "/chunk/upload");
+  XHR.send(formData);
+};
