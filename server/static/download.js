a6a5e4b64dcb9ac441802973323818da31f170b1
Daniel <97473619+daniel-cavalcante@users.noreply.github.com>
2023-06-17 20:38:00 -0300

    Change storageFilesList to bucketList

--- server/static/download.js
+++ server/static/download.js
@@ -1,22 +1,22 @@
 const DOWNLOAD_URL = "http://localhost:5000/download";
 
 const getFilesList = () => {
-  const storedFilesList = document.getElementById("storedFilesList");
+  const bucketList = document.getElementById("bucketList");
   XHR = new XMLHttpRequest();
 
   // on success, append the name of stored files on <ol> list element
   XHR.addEventListener("load", (event) => {
-    const storedFiles = JSON.parse(event.target.response);
+    const bucket = JSON.parse(event.target.response);
 
-    for (let index = 0; index < storedFiles.length; index++) {
+    for (let index = 0; index < bucket.length; index++) {
       // anchor element referencing specific file download url
       a = document.createElement("a");
-      a.setAttribute("href", DOWNLOAD_URL + "/" + storedFiles[index]);
-      linkText = document.createTextNode(storedFiles[index]);
+      a.setAttribute("href", DOWNLOAD_URL + "/" + bucket[index]);
+      linkText = document.createTextNode(bucket[index]);
       a.appendChild(linkText);
 
       // append anchor element to the list as a list item
-      item = storedFilesList
+      item = bucketList
         .appendChild(document.createElement("li"))
         .appendChild(a);
     }
