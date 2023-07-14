8bac744db305fa222f357b2f8cae13b791f1b4cd
Daniel Cavalcante <97473619+daniel-cavalcante@users.noreply.github.com>
2023-07-14 16:46:26 -0300

    Create docker-compose.yml

--- source/storage.py
+++ source/storage.py
@@ -2,11 +2,12 @@ import os
 
 from minio import Minio
 
-
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT") or "localhost:9000"
-MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY") or "minioadmin"
-MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY") or "minioadmin"
-MINIO_IS_SECURE = os.getenv("MINIO_IS_SECURE") or "False"
+MINIO_HOSTNAME=os.getenv("MINIO_HOSTNAME")
+MINIO_PORT=os.getenv("MINIO_PORT")
+MINIO_ENDPOINT = f"{MINIO_HOSTNAME}:{MINIO_PORT}"
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+MINIO_IS_SECURE = os.getenv("MINIO_IS_SECURE")
 
 storage = Minio(
     endpoint=MINIO_ENDPOINT,
