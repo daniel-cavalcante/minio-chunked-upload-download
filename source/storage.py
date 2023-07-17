6df9a165e82ee843321f072feb2c8884996f55fa
Daniel Cavalcante <97473619+daniel-cavalcante@users.noreply.github.com>
2023-07-17 16:54:55 -0300

    Fix 'None' from os.getenv(env_var)

--- source/storage.py
+++ source/storage.py
@@ -1,9 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 from minio import Minio
+
+load_dotenv()
 
 MINIO_HOSTNAME=os.getenv("MINIO_HOSTNAME")
 MINIO_PORT=os.getenv("MINIO_PORT")
+print(MINIO_PORT)
 MINIO_ENDPOINT = f"{MINIO_HOSTNAME}:{MINIO_PORT}"
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
